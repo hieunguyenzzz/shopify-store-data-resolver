b0e46e8acf89beb06527654cc1833063e3a4cf68
Hieu Nguyen <hieunguyenel@gmail.com>
2025-07-03 10:54:03 +0700

    Increase metafields limit in fetchAllProducts function from 30 to 35 to enhance data retrieval capabilities.

--- app/services/shopify.service.ts
+++ app/services/shopify.service.ts
@@ -455,7 +455,7 @@ export async function fetchAllProducts(): Promise<any[]> {
             name
             values
           }
-          metafields(first: 30) {
+          metafields(first: 35) {
             nodes {
               namespace
               key
